e8df3b49666d8cc5c0ea80c15053a67e63742000
Alexandra LEE <palexalee@gmail.com>
2018-04-23 16:29:43 +0200

    add final

--- homepage.js
+++ homepage.js
@@ -2,31 +2,33 @@
 
  var db = firebase.database();
  const ref = db.ref("zombies");
- 
 
- 
-    ref.once("value", function (snapshot) {     
-        snapshot.forEach(function (childSnapshot) {
-            let childData = childSnapshot.val(); 
-                   
-        document.getElementById('list-zombies').innerHTML += '<li>' + childData.name + '</li>';          
-        
-    }); 
-})
-    
-function writeUserData(Name) {
-    db.ref('zombies/' + name).push({
-      name: Name,
-      
-    });
-  }
 
-  function submitClick() {
-    let name = document.getElementById("Name").value;
-    writeUserData(name);
-    window.alert("Beware! Zombie population increasing.");
-    window.location.reload();
-    
-}
- 
+ ref.once("value", function (snapshot) {
+     let user = firebase.auth().currentUser;
+     snapshot.forEach(function (childSnapshot) {
+         let childData = childSnapshot.val();
 
+         if (!childData.creator || childData.creator == user.uid) {
+             document.getElementById('list-zombies').innerHTML += '<li>' + childData.name + '</li>';
+         }
+     });
+ })
+
+ function writeUserData(Name) {
+     let user = firebase.auth().currentUser;
+
+     db.ref('zombies/' + name).push({
+         name: Name,
+         creator: user.uid
+
+     });
+ }
+
+ function submitClick() {
+     let name = document.getElementById("Name").value;
+     writeUserData(name);
+     window.alert("Beware! Zombie population increasing.");
+     window.location.reload();
+
+ }
